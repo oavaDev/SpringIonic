ffd151704b99c24bea141880db2c4b24c5aa2307
cCeth <omarvega598@gmail.com>
2023-03-02 13:33:53 -0500

    add test restController

--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -1,4 +1,20 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CustomerController {
+
+    @GetMapping("/api/customers")
+    public List<Customer> getAll() {
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer());
+        return customers;
+    }
+
 }
