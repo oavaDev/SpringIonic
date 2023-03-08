3b6aa02e540ed2e0dd9b4098d5f3e0765adb8599
cCeth <omarvega598@gmail.com>
2023-03-08 17:58:45 -0500

    fix cors

--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -13,6 +13,7 @@ public class CustomerController {
     private ICostumerService service;
 
     @GetMapping("/api/customers")
+    @CrossOrigin(origins = "http://localhost:3000")
     public List<Customer> getAll() {
         return service.getAll();
     }
